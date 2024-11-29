943daa372a95a754f63bfc0d5437ad34f570887d
LeonardUlander <3kdyhfed24.ulanle@folkuniversitetet.nu>
2024-11-30 01:00:29 +0200

    lagt till concept-sidan och gjort klart den för mobile. Även gjort ytterligare styling på headern och footern. Där inkluderat lite javscript.

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function openHamburger() {
 function closeHamburger() {
   document.getElementById("hamburger").style.width = "0%";
 }
+
+$(document).on('scroll', function(){
+  if ( $(window).scrollTop() > 100) {
+      $('header').addClass('header--not-transparent');
+  } else {
+      $('header').removeClass('header--not-transparent');
+  }
+});
+
+$(document).ready(function() {
+  
+  $(window).scroll(function() {
+    
+    if ($(window).scrollTop() > 2100) {
+      $('header').addClass('header--not-fixed');
+    } else {
+      $('header').removeClass('header--not-fixed');
+    };
+  });
+});
